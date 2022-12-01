0116643e78f4599e3128d4062ebd220af29df6a9
Stephanie <63380487+stephlojano@users.noreply.github.com>
2022-12-01 09:43:22 -0500

    Update amazonwebscraper.py

--- amazonwebscraper.py
+++ amazonwebscraper.py
@@ -7,7 +7,8 @@ csv_header = ['title', 'price', 'rating', 'no. of reviews', 'product URL', 'bran
 headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/105.0.0.0 Safari/537.36","Accept-Encoding": "gzip, deflate","Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9","DNT":"1","Connection":"close", "Upgrade-Insecure-Requests":"1"}
 
 # searching for...
-query = 'iphone 12 cases'
+# enter product inside ''
+query = ''
 
 # format search
 search = query.replace(' ', '+')
